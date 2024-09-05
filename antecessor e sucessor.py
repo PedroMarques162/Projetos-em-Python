d18d3f2f37496f1be0f88a629a76b9d0bebc1aff
Pedro Marques <pedropaulomarquesda@gmail.com>
2024-09-05 00:30:16 -0300

    Add files via upload

---
+++ antecessor e sucessor.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um valor:'))
+a = n-1
+s = n+1
+
+print('O número inserido {}, o seu antecesor é {} e o seu sucessor é {}'.format(n,a,s))
---
+++ dobro,triplo e raiz quadrada.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um valor:'))
+
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+
+print('O dobro de {} é {}, o triplo é {} e a sua raiz quadrada é {}'.format(n,d,t,r))
